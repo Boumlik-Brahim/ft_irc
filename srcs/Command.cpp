1f5cce63b2cb550443cea170b8abf75100cc9592
Boumlik-Brahim <boumlik01brahim@gmail.com>
2023-02-03 10:46:42 +0100

    Add: new-branch-command

--- srcs/Command.cpp
+++ srcs/Command.cpp
@@ -6,11 +6,68 @@
 /*   By: bbrahim <bbrahim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 11:22:27 by bbrahim           #+#    #+#             */
-/*   Updated: 2023/02/03 10:09:01 by bbrahim          ###   ########.fr       */
+/*   Updated: 2023/02/03 10:45:55 by bbrahim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/Server.hpp"
+
+void sendMessage(int fd, std::string message)
+{
+	size_t i = 0;
+	while (i != message.length())
+		i += send(fd, message.c_str(), message.length() - i, 0);
+}
+
+// void checkPrivateMsg(int senderFd, Message &msg)
+// {
+// 	// (void)senderFd;
+
+// 	std::string receiver = msg.getArgument()[0];
+// 	// std::cout << "Receiver == " << receiver << std::endl;
+// 	/* check if receiver is a client */
+// 	// std::cout << "size --" << msg.getArgument().size() << std::endl;
+// 	/* reply sender with ERR_NOTEXTTOSEND message */
+// 	if (msg.getArgument().size() < 2)
+// 		sendMessage(senderFd, ERR_NOTEXTTOSEND);
+
+// 	for (unsigned long i = 1; i < msg.getArgument().size(); i++)
+// 		std::cout << "-- " << msg.getArgument()[i] << std::endl;
+
+// 	std::string newMessage;
+// 	// char **newMessage;
+// 	std::cout << "--------" << msg.getArgument()[1][0] << std::endl;
+// 	// if (msg.getArgument()[1].c_str() == ":")
+// 	// {
+// 	//     int n = 0;
+
+// 	// }
+// }
+
+// void checkNotice(Message &msg, int sender_fd)
+// {
+// 	(void)sender_fd;
+// 	std::vector<std::string> clients;
+// 	std::string msgtarget;
+
+// 	msgtarget = msg.getArgument()[0];
+// 	std::cout << "msgtarget is ====>" << msgtarget << std::endl;
+// 	for (std::map<int, Client *>::iterator it = mapClients.begin(); it != mapClients.end(); ++it)
+// 	{
+// 		std::cout << "msgargument is ====>" << it->second << std::endl;
+// 		if (msgtarget != it->second->getNickName())
+// 		{
+// 			std::cout << "makaynch";
+// 			sendMessage(sender_fd, ERR_NORECIPIENT);
+// 			break;
+// 		}
+// 	}
+// 	if (msg.getArgument().size() < 2)
+// 	{
+// 		sendMessage(sender_fd, ERR_NOTEXTTOSEND);
+// 		return;
+// 	}
+// }
 
 void parseMessageFormat(Message &msg, char **data)
 {
