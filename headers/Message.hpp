92ae253d4b304814f26955b2a3a53a4e8b329838
Boumlik-Brahim <boumlik01brahim@gmail.com>
2023-02-03 10:10:40 +0100

    Edit: setargs and setcmd

--- headers/Message.hpp
+++ headers/Message.hpp
@@ -6,7 +6,7 @@
 /*   By: bbrahim <bbrahim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 13:35:17 by bbrahim           #+#    #+#             */
-/*   Updated: 2023/02/02 15:08:52 by bbrahim          ###   ########.fr       */
+/*   Updated: 2023/02/03 09:47:13 by bbrahim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,26 +22,21 @@ class Message
 	private:
 		std::vector<std::string>	_arguments;
 		std::string					_command;
-		std::string					_appendMsg;
 		bool						_isValidCommad;
 
 	public:
 		Message();
 		Message(const Message &copy);
 		Message &operator=(const Message &assign);
+
+		std::vector<std::string>	getArgument(void) const;
+		void						setArguments(std::vector<std::string> arguments);
+		std::string					getCommand(void) const;
+		void						setCommand(std::string command);
+		bool						getIsValidCommand(void) const;
+		void						setIsValidCommand(bool isValid);
+
 		~Message();
-
-		void setArguments(char **data);
-		void setCommand(char *cmd);
-		void setIsValidCommand(bool isValid);
-		void setAppendMsg(std::string toAppend);
-
-		std::string getAppendMsg() const;
-		std::string getCommand(void) const;
-		bool 		getIsValidCommand(void) const;
-		std::vector<std::string> getArgument(void) const;
-
-		
 };
 
 #endif
