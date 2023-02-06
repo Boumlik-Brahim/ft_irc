f1368bdcd0c657c128aedcb164d3f5cb68ebd054
Boumlik-Brahim <boumlik01brahim@gmail.com>
2023-02-06 15:30:16 +0100

    Add: handlejoincmd function to commands

--- srcs/Command.cpp
+++ srcs/Command.cpp
@@ -6,7 +6,7 @@
 /*   By: bbrahim <bbrahim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 11:22:27 by bbrahim           #+#    #+#             */
-/*   Updated: 2023/02/06 11:38:44 by bbrahim          ###   ########.fr       */
+/*   Updated: 2023/02/06 15:29:04 by bbrahim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,6 +77,40 @@ void  Server::handlePrivmsgCmd(Message &msg, int senderFd)
     sendMessage(receiverFd, messageFormat);
 }
 
+void  Server::handleJoinCmd(Message &msg, Channel &chnl, int senderFd)
+{
+    std::string	receiver;
+    std::string	channelName;
+	std::string	cmd;
+
+	(void)senderFd;
+	receiver = msg.getArgument()[0];
+	channelName = chnl.getChannelName();
+	cmd = msg.getCommand();
+	// if ()/*ERR_NEEDMOREPARAMS*/
+	// 	return (errorHandler(senderFd, 461, cmd));
+	// else if ()/*ERR_INVITEONLYCHAN*/
+	// 	return (errorHandler(senderFd, 473, channelName));
+	// else if ()/*ERR_CHANNELISFULL*/
+	// 	return (errorHandler(senderFd, 471, channelName));
+	// else if ()/*ERR_NOSUCHCHANNEL*/
+	// 	return (errorHandler(senderFd, 403, channelName));
+	// else if ()/*ERR_TOOMANYTARGETS*/
+	// 	return (errorHandler(senderFd, 407, receiver, "abort message"));
+	// else if ()/*ERR_BANNEDFROMCHAN*/
+	// 	return (errorHandler(senderFd, 474, channelName));
+	// else if ()/*ERR_BADCHANNELKEY*/
+	// 	return (errorHandler(senderFd, 475, channelName));
+	// else if ()/*ERR_BADCHANMASK*/
+	// 	return (errorHandler(senderFd, 476, channelName));
+	// else if ()/*ERR_TOOMANYCHANNELS*/
+	// 	return (errorHandler(senderFd, 405, channelName));
+	// else if ()/*ERR_UNAVAILRESOURCE*/
+	// 	return (errorHandler(senderFd, 437, receiver, channelName));
+	// else if ()/*RPL_TOPIC*/
+	// 	return (cmd_Resp_Handler(senderFd, 332, channelName, "topic"));
+}
+
 void  Server::handleNoticeCmd(Message &msg, int senderFd)
 {
     std::string receiver;
@@ -141,6 +175,7 @@ void parseMessageFormat(Message &msg, char **data)
 void Server::backBone(std::string buffer, int newSocketFd)
 {
 	Message msg;
+	Channel chnl;
 	char 	**data;
 
 	data = ft_split(buffer.c_str(), ' ');
@@ -160,7 +195,7 @@ void Server::backBone(std::string buffer, int newSocketFd)
 	else if (!msg.getCommand().compare("SQUIT"))
 	    std::cout << "i got the squit" << std::endl;
 	else if (!msg.getCommand().compare("JOIN"))
-	    std::cout << "i got the join" << std::endl;
+		handleJoinCmd(msg, chnl, newSocketFd);
 	else if (!msg.getCommand().compare("PART"))
 	    std::cout << "i got the part" << std::endl;
 	else if (!msg.getCommand().compare("MODE"))
