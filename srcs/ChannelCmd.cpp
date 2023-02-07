3ac73b4cbdc06a0e50035edde88adb2eec2e88e6
ishakzail <ishak.zail.iz@gmail.com>
2023-02-07 19:05:10 +0100

    new

--- srcs/ChannelCmd.cpp
+++ srcs/ChannelCmd.cpp
@@ -1,28 +1,87 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   channelCmd.cpp                                     :+:      :+:    :+:   */
+/*   ChannelCmd.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bbrahim <bbrahim@student.42.fr>            +#+  +:+       +#+        */
+/*   By: izail <izail@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 09:39:29 by bbrahim           #+#    #+#             */
-/*   Updated: 2023/02/07 10:09:17 by bbrahim          ###   ########.fr       */
+/*   Updated: 2023/02/07 18:58:08 by izail            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/Server.hpp"
 
-void  Server::handleJoinCmd(Message &msg, Channel &chnl, int senderFd)
+int	Server::findChannelByName(std::string channelName)
+{
+	for(size_t i = 0; i < _channels.size(); i++)
+	{
+		if (channelName == _channels.at(i).getChannelName())
+			return (1);
+	}
+	return (0);
+}
+
+void	Server::createChannel(Channel &chnl, int senderFd, std::string channelName)
+{
+    std::map<int, Client *>::iterator it = _mapClients.find(senderFd);
+	std::string creator = findNickClientByFd(senderFd);
+	chnl.setChannelCreator(creator);
+	chnl.setChannelName(channelName);
+
+	chnl.setChannelMembers(it->second->getNickName());
+	chnl.setChannelOperators(it->second->getNickName());
+	chnl.setChannelModes("n");
+	_channels.push_back(chnl);
+}
+
+void  Server::handleJoinCmd(Message &msg, int senderFd)
 {
     std::string	receiver;
-    std::string	channelName;
+	Channel 	tmpChannel;
 	std::string	cmd;
+	int	channelExist;
 
-	(void)senderFd;
-	receiver = msg.getArguments()[0];
-	channelName = chnl.getChannelName();
-	cmd = msg.getCommand();
-
+	// check if JOIN command have multiple channels
+	if (!msg.getMultiArgs().empty())
+	{
+		for (size_t i = 0; i < msg.getMultiArgs().size(); i++)
+		{
+			channelExist = findChannelByName(msg.getMultiArgs().at(i));
+			if (channelExist)
+			{
+				// check mode;
+				// Join user to channel
+			}
+			else
+			{
+				// create new Channel
+				// set user as the operator of this channel
+			}
+		}
+	}
+	else
+	{
+		channelExist = findChannelByName(msg.getArguments().at(0));
+		if (channelExist)
+		{
+			// check mode
+			// join user to channel
+		}
+		else
+		{
+			createChannel(tmpChannel, senderFd, msg.getArguments().at(0));
+			// create new channel
+			/* 
+				createChannel(chnl, senderFd, msg.getArguments()[0]);
+				string user = getUserByFd(senderFd);
+				user.getJoinedChannel().push_back("channelName");
+				setMode();
+				
+			*/
+			// set user as the operator of this channel
+		}
+	}
 	// if ()/*ERR_NEEDMOREPARAMS*/
 	// 	return (errorHandler(senderFd, 461, cmd));
 	// else if ()/*ERR_INVITEONLYCHAN*/
