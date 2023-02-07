c5d82a6a86fe6b928f48aaac7a815a5db4b0e5d9
ishakzail <ishak.zail.iz@gmail.com>
2023-02-07 19:02:21 +0100

    update: add _joinedChann vector and update getters

--- headers/Channel.hpp
+++ headers/Channel.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bbrahim <bbrahim@student.42.fr>            +#+  +:+       +#+        */
+/*   By: izail <izail@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 18:20:09 by bbrahim           #+#    #+#             */
-/*   Updated: 2023/02/06 17:29:34 by bbrahim          ###   ########.fr       */
+/*   Updated: 2023/02/07 18:54:27 by izail            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,26 +20,35 @@
 # include <string>
 # include <map>
 # include "../headers/Client.hpp"
+
 class Channel
 {
 	private:
-		std::string					_channelName;
-		std::string					_channelCreator;
-		std::vector<Client>			_channelMembers;
-		std::vector<Client>			_channelOperators;
-		std::vector<Client>			_channelBannedMembers;
-		std::vector<std::string>	_channelModes;
+		std::string								_channelName;
+		std::string								_channelCreator;
+		std::vector<std::string>				_channelMembers;
+		std::vector<std::string>				_channelOperators;
+		std::vector<std::string>				_channelBannedMembers;
+		std::vector<std::string>				_channelModes;
 
 	public:
 		Channel();
 		Channel(const Channel &copy);
-		Channel & operator=(const Channel &assign);
+		// Channel & operator=(const Channel &assign);
 
-		std::string	getChannelName(void) const;
-		void		setChannelName(std::string channelName);
-		std::string	getChannelCreator(void) const;
-		void		setChannelCreator(std::string channelCreator);
-
+		std::string&				getChannelName(void) ;
+		void						setChannelName(std::string channelName);
+		std::string&				getChannelCreator(void);
+		void						setChannelCreator(std::string channelCreator);
+		std::vector<std::string>& 	getChannelMembers(void);
+        void 						setChannelMembers(std::string channelMember);
+        std::vector<std::string>& 	getChannelOperators(void);
+        void        				setChannelOperators(std::string channelOperator);
+        std::vector<std::string>&	getChannelBannedMembers(void);
+        void        				setChannelBannedMembers(std::string channelBannedMember);
+        std::vector<std::string>& 	getChannelModes(void);
+        void        				setChannelModes(std::string channelModes);
+		
 		~Channel();
 };
 
