4becb9ce94dc5f099c7dcc70b1c7d6a118d2828d
ishakzail <ishak.zail.iz@gmail.com>
2023-02-07 19:04:59 +0100

    update: edit getters and setters in Channel.hpp

--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bbrahim <bbrahim@student.42.fr>            +#+  +:+       +#+        */
+/*   By: izail <izail@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 18:23:31 by bbrahim           #+#    #+#             */
-/*   Updated: 2023/02/06 17:27:04 by bbrahim          ###   ########.fr       */
+/*   Updated: 2023/02/07 18:53:55 by izail            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,23 @@ Channel::Channel()
 {}
 Channel::Channel(const Channel &copy)
 {
-	(void) copy;
-	std::cout << "\e[0;33mCopy Constructor called of Channel\e[0m" << std::endl;
+	*this = copy;
 }
-Channel & Channel::operator=(const Channel &assign)
-{
-	(void) assign;
-	return *this;
-}
+// Channel & Channel::operator=(const Channel &assign)
+// {
+// 	(void) assign;
+// 	return *this;
+// }
 
-std::string	Channel::getChannelName(void) const
+std::string&	Channel::getChannelName(void)
 {
 	return(_channelName);
 }
 void	Channel::setChannelName(std::string channelName)
 {
 	_channelName = channelName;
-
 }
-std::string	Channel::getChannelCreator(void) const
+std::string&	Channel::getChannelCreator(void)
 {
 	return(_channelCreator);
 }
@@ -43,6 +41,38 @@ void	Channel::setChannelCreator(std::string channelCreator)
 	_channelCreator = channelCreator;
 }
 
+std::vector<std::string>& Channel::getChannelMembers(void)
+{
+    return (_channelMembers);
+}
+void Channel::setChannelMembers(std::string channelMember)
+{
+    _channelMembers.push_back(channelMember);
+}
+std::vector<std::string>& Channel::getChannelOperators(void)
+{
+    return (_channelOperators);
+}
+void Channel::setChannelOperators(std::string channelOperator)
+{
+    _channelOperators.push_back(channelOperator);
+}
+std::vector<std::string>& Channel::getChannelBannedMembers(void)
+{
+    return (_channelBannedMembers);
+}
+void Channel::setChannelBannedMembers(std::string channelBannedMember)
+{
+    _channelBannedMembers.push_back(channelBannedMember);
+}
+std::vector<std::string>& Channel::getChannelModes(void)
+{
+    return (_channelModes);
+}
+void Channel::setChannelModes(std::string channelMode)
+{
+    _channelModes.push_back(channelMode);
+}
 
 Channel::~Channel()
 {}
