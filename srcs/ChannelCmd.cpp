9e4c69923c9f33b9a22ade7d619ff92231fe7e2c
Boumlik-Brahim <boumlik01brahim@gmail.com>
2023-02-08 19:00:02 +0100

    Edit: handleJoincmd function

--- srcs/ChannelCmd.cpp
+++ srcs/ChannelCmd.cpp
@@ -6,7 +6,7 @@
 /*   By: bbrahim <bbrahim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 09:39:29 by bbrahim           #+#    #+#             */
-/*   Updated: 2023/02/08 15:04:12 by bbrahim          ###   ########.fr       */
+/*   Updated: 2023/02/08 18:39:46 by bbrahim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,20 +42,25 @@ void	Server::createChannel(Channel &chnl, int senderFd, std::string channelName)
 	chnl.setChannelMembers(it->second->getNickName());
 	chnl.setChannelOperators(it->second->getNickName());
 	chnl.setChannelModes("n");
+	it->second->setJoinedChannels(channelName);
 	_channels.push_back(chnl);
 }
 void  Server::handleJoinCmd(Message &msg, int senderFd)
 {
-    std::string	receiver;
-    std::string	channelName;
-	std::string	cmd;
-	Channel		tmpChannel;
-	Channel		tmp;
-	int			channelExist;
+    std::string							receiver;
+    std::string							channelName;
+	std::string							cmd;
+	Channel								tmpChannel;
+	Channel								tmp;
+	int									channelExist;
+    std::map<int, Client *>::iterator	it;
 
-	// check if JOIN command have multiple channels
+	it = _mapClients.find(senderFd);
+	cmd = msg.getCommand();
 	checkMultiArgs(msg);
 	checkChnlNames(msg.getMultiArgs(), senderFd);
+	if (msg.getMultiArgs().empty() && msg.getArguments().at(0).empty()) /*ERR_NEEDMOREPARAMS*/
+		return (errorHandler(senderFd, 461, cmd));
 	if (!msg.getMultiArgs().empty())
 	{
 		if (msg.getMultiArgs().size() > 2)/*ERR_TOOMANYTARGETS*/
@@ -63,35 +68,21 @@ void  Server::handleJoinCmd(Message &msg, int senderFd)
 		for (size_t i = 0; i < msg.getMultiArgs().size(); i++)
 		{
 			channelName = msg.getMultiArgs().at(i);
-			// if (!findChannelByName(channelName))/*ERR_NOSUCHCHANNEL*/
-			// 	return (errorHandler(senderFd, 403, channelName));
+			if (!findChannelByName(channelName))/*ERR_NOSUCHCHANNEL*/
+				return (errorHandler(senderFd, 403, channelName));
 			channelExist = findChannelByName(channelName);
 			if (channelExist)
 			{
 				tmp = findChannel(channelName);
 				if (tmp.getChannelMembers().size() > 9)/*ERR_CHANNELISFULL*/
 					return (errorHandler(senderFd, 471, tmp.getChannelName()));
-				// for(size_t i = 0; i < tmp.getChannelModes().size() ; i++)
-				// {
-				// 	if (tmp.getChannelModes().at(i) == "a")
-				// 		void checkAnonymousChanFlag();
-				// 	else if (tmp.getChannelModes().at(i) == "i")
-				// 		void checkInviteOnlyChanFlag();
-				// 	else if (tmp.getChannelModes().at(i) == "m")
-				// 		void checkModeratedChan();
-				// 	else if (tmp.getChannelModes().at(i) == "n")
-				// 		void checkNoMsgToChannel();
-				// 	else if (tmp.getChannelModes().at(i) == "q")
-				// 		void checkQuietChanFlag();
-				// 	else if (tmp.getChannelModes().at(i) == "p")
-				// 		void PrivateChanFlag();
-				// 	else if (tmp.getChannelModes().at(i) == "s")
-				// 		void checkSecretChanFlag();
-				// 	else if (tmp.getChannelModes().at(i) == "r")
-				// 		void checkServerReopChanFlag();
-				// 	else if (tmp.getChannelModes().at(i) == "t")
-				// 		void checkTopic();
-				// }
+				else if (it->second->getJoinedChannels().size() > 2)/*ERR_TOOMANYCHANNELS*/
+					return (errorHandler(senderFd, 405, channelName));
+				for(size_t i = 0; i < tmp.getChannelModes().size() ; i++)
+				{
+					if (tmp.getChannelModes().at(i) == "i")
+						checkInviteOnlyChanFlag(tmp, it->second->getNickName(), senderFd);
+				}
 			}
 			else
 				createChannel(tmpChannel, senderFd, msg.getArguments().at(i));
@@ -102,14 +93,10 @@ void  Server::handleJoinCmd(Message &msg, int senderFd)
 		channelExist = findChannelByName(msg.getArguments().at(0));
 		if (channelExist)
 		{
-			// check mode
-			// join user to channel
 		}
 		else
 			createChannel(tmpChannel, senderFd, msg.getArguments().at(0));
 	}
-	if (msg.getMultiArgs().empty() && msg.getArguments().at(0).empty()) /*ERR_NEEDMOREPARAMS*/
-		return (errorHandler(senderFd, 461, cmd));
 	// else if ()/*ERR_INVITEONLYCHAN*/
 	// 	return (errorHandler(senderFd, 473, channelName));
 	// else if ()/*ERR_BANNEDFROMCHAN*/
@@ -118,10 +105,6 @@ void  Server::handleJoinCmd(Message &msg, int senderFd)
 	// 	return (errorHandler(senderFd, 475, channelName));
 	// else if ()/*ERR_BADCHANMASK*/
 	// 	return (errorHandler(senderFd, 476, channelName));
-	// else if ()/*ERR_TOOMANYCHANNELS*/
-	// 	return (errorHandler(senderFd, 405, channelName));
-	// else if ()/*ERR_UNAVAILRESOURCE*/
-	// 	return (errorHandler(senderFd, 437, receiver, channelName));
 	// else if ()/*RPL_TOPIC*/
 	// 	return (cmd_Resp_Handler(senderFd, 332, channelName, "topic"));
 }
