069556b4ff5b3d0f919176541db13c55d7b6506a
ishakzail <ishak.zail.iz@gmail.com>
2023-02-14 11:02:10 +0100

    update : add new constructor for _servername and WelcomeMsg function to reply user when logged in

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -6,13 +6,15 @@
 /*   By: izail <izail@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 16:41:32 by bbrahim           #+#    #+#             */
-/*   Updated: 2023/02/08 11:24:53 by izail            ###   ########.fr       */
+/*   Updated: 2023/02/14 09:48:28 by izail            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/Server.hpp"
 
 Server::Server()
+{}
+Server::Server(std::string server_name, int port_number, std::string password) : _server_name(server_name),  _password(password), _port_number(port_number)
 {}
 Server::Server(int port_number, std::string password) : _password(password), _port_number(port_number)
 {}
@@ -194,6 +196,14 @@ void Server::read_write_socket(int newSocketFd, int *count)
 void Server::close_socket(int socket_fd)
 {
 	close(socket_fd);
+}
+
+void Server::WelcomeMsg(int fd)
+{
+	std::string sender = findNickClientByFd(fd);
+	cmd_Resp_Handler1(fd, 1, _server_name, sender);
+	cmd_Resp_Handler1(fd, 2, _server_name, sender);
+	cmd_Resp_Handler1(fd, 3, _server_name, sender);
 }
 
 // Destructor
