9ac31bff0c1093835a05ac09da89ce3483667f1b
ishakzail <ishak.zail.iz@gmail.com>
2023-02-17 15:19:37 +0100

    update : add splitBySep in Utils header

--- headers/Utils.hpp
+++ headers/Utils.hpp
@@ -6,7 +6,7 @@
 /*   By: izail <izail@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 11:20:15 by bbrahim           #+#    #+#             */
-/*   Updated: 2023/02/08 10:13:19 by izail            ###   ########.fr       */
+/*   Updated: 2023/02/16 13:42:26 by izail            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@
 #include "Server.hpp"
 
 
-char	**ft_split(const char *s, char c);
-void    checkMultiArgs(Message &msg);
-void    checkChnlNames(std::vector<std::string> tmpArgs, int newSocketFd);
-
+char	                    **ft_split(const char *s, char c);
+void                        checkMultiArgs(Message &msg);
+void                        checkChnlNames(std::vector<std::string> tmpArgs, int newSocketFd);
+std::vector<std::string>    splitBySeparator(std::string args, std::string sep);
 
 #endif
