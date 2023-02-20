b5c1fcd7e0d3d7d20e1f13390662e6dd47744bdc
Boumlik-Brahim <boumlik01brahim@gmail.com>
2023-02-20 17:32:03 +0100

    Edit: checkChnlNames prototype

--- headers/Utils.hpp
+++ headers/Utils.hpp
@@ -6,7 +6,7 @@
 /*   By: bbrahim <bbrahim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 18:57:02 by bbrahim           #+#    #+#             */
-/*   Updated: 2023/02/17 18:57:04 by bbrahim          ###   ########.fr       */
+/*   Updated: 2023/02/20 16:37:56 by bbrahim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@
 
 char	                    **ft_split(const char *s, char c);
 void                        checkMultiArgs(Message &msg);
-void                        checkChnlNames(std::vector<std::string> tmpArgs, int newSocketFd);
+void                        checkChnlNames(std::vector<std::string> tmpArgs);
 std::vector<std::string>    splitBySeparator(std::string args, std::string sep);
 
 
