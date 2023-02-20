0c5464c171049025eee61d197cd6fc515bcb6bb5
ishakzail <ishak.zail.iz@gmail.com>
2023-02-20 16:12:50 +0100

    update : add func sendReplay() in Utils.hpp

--- headers/Utils.hpp
+++ headers/Utils.hpp
@@ -3,11 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   Utils.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iomayr <iomayr@student.42.fr>              +#+  +:+       +#+        */
+/*   By: izail <izail@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 11:20:15 by bbrahim           #+#    #+#             */
+/*   Updated: 2023/02/19 17:32:40 by izail            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #ifndef UTILS_HPP
 #define UTILS_HPP
@@ -23,6 +25,6 @@ char	                    **ft_split(const char *s, char c);
 void                        checkMultiArgs(Message &msg);
 void                        checkChnlNames(std::vector<std::string> tmpArgs, int newSocketFd);
 std::vector<std::string>    splitBySeparator(std::string args, std::string sep);
-
+void                        sendReplay(int fd, std::string message);
 
 #endif
